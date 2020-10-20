33da5a904b9d4924f8d20d9170cf4034f59c26e0
RaginiKulkarni14 <raginikulkarni10@gmail.com>
2020-10-20 14:03:00 +0530

    scdf-commit

---
+++ SpringCloudDataFlow-for-Trade/trade-processor/src/test/java/com/ofss/daytrader/TradeProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofss.daytrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeProcessorApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
